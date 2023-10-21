18f6349aaba86a228dfcf2707d0d19a9cb2d18aa
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-21 11:32:01 +0530

    added mapping to the event card in upcoming Events

--- src/components/Eventcard.js
+++ src/components/Eventcard.js
@@ -1,28 +1,27 @@
-import EventImage from "../assests/event-image.png";
 import LocationIcon from "../assests/location.svg";
 import ClockIcon from "../assests/clock.svg";
 import ArrowRight from "../assests/arrow-right.svg";
 
-const EventCard = () => {
+const EventCard = ({eventImage, location, time, eventName}) => {
     return (
         <div >
             <div className="flex flex-col w-[345px] h-[345px] p-4 rounded-3xl outline absolute bg-white outline-secondary outline-2">
                 {/* Event Image */}
-                <img src={EventImage} alt="event" className="rounded-t-lg" />
+                <img src={eventImage} alt="event" className="rounded-t-lg" />
                 {/* Event Info */}
                 <div className="flex flex-row gap-2 items-center w-full mt-1">
                     {/* Location */}
                     <div className="flex flex-row items-center gap-0.5">
                         <img src={LocationIcon} alt="location" className="h-6 w-6" />
-                        <div className="font-outfit text-center text-black text-sm font-normal">Aeronautical Department</div>
+                        <div className="font-outfit text-center text-black text-sm font-normal">{location}</div>
                     </div>
                     <div className="flex flex-row items-center gap-0.5">
                         <img src={ClockIcon} alt="location" className="h-6 w-6" />
-                        <div className="font-outfit text-center text-black text-sm font-normal">10:45 - 11:45 </div>
+                        <div className="font-outfit text-center text-black text-sm font-normal">{time}</div>
                     </div>
                 </div>
                 {/* Event Title */}
-                <div className="font-outfit items-start text-primary text-lg font-bold mt-2">Robotics Prototype Workshop </div>
+                <div className="font-outfit items-start text-primary text-lg font-bold mt-2">{eventName}</div>
                 {/* Event Registration */}
                 <div className="flex flex-row justify-center items-center mt-3 rounded-xl hover:bg-slate-200 p-1 duration-100 ease-out w-36     hover:scale-105 cursor-pointer">
                     <div className="font-outfit text-black text-md font-semibold">Register Now</div>
--- src/components/Upcomingevents.js
+++ src/components/Upcomingevents.js
@@ -1,13 +1,19 @@
 import EventCard from "./Eventcard";
+import event1 from "../assests/event-image.png"
 
 const UpcomingEvents = () => {
+    const events = [
+        {eventImage: event1, location: "Aeronautical Auditorium", time: "10:00 AM", eventName: "Robotics Workshop"},
+        {eventImage: event1, location: "Aeronautical Auditorium", time: "10:00 AM", eventName: "Robotics Workshop"},
+        {eventImage: event1, location: "Aeronautical Auditorium", time: "10:00 AM", eventName: "Robotics Workshop"}
+    ];
     return (
         <div className="flex flex-col items-center mt-20">
             <div className="font-outfit text-4xl text-primary font-bold">Upcoming <span className="text-secondary">Events</span></div>
             <div className="flex flex-row justify-between items-center gap-2.5 mt-10">
-                <EventCard />
-                <EventCard />
-                <EventCard />
+                {events.map((event, index) => (
+                    <EventCard key={index} eventImage={event.eventImage} location={event.location} time={event.time} eventName={event.eventName} />
+                ))}
             </div>
         </div>
     );
