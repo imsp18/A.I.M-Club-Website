eed02bfec6d13c90021c88871cdc2ca084037002
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-23 19:12:22 +0530

    added a Teamsection to the website

--- src/components/Teamcard.js
+++ src/components/Teamcard.js
@@ -1,7 +1,26 @@
+import X from "../assests/X-logo.svg";
+import Linkedin from "../assests/linkedIn-logo.svg";
+
 const TeamCard = () => {
-    return ( 
-        <div className=""></div>
-     );
+    return (
+        <div className="flex flex-col items-center justify-center ">
+            <div className="h-[169px] w-[169px] bg-white absolute z-10 mb-60 border-primaryLight border rounded-full"></div>
+            <div className="flex flex-col px-8 justify-center items-center w-[345px] h-[233px] absolute border border-primaryLight bg-white rounded-2xl">
+                {/* Name */}
+                <div className="font-outfit text-lg text-primaryLight font-semibold mt-12 uppercase">name</div>
+                {/* Description */}
+                <div className="font-outfit text-justify text-sm text-black font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+                {/* Social Media */}
+                <div className="flex flex-row gap-4 mt-4">
+                    <img src={Linkedin} alt="X logo" className="h-5 w-5 hover:scale-110 cursor-pointer hover:co" />
+                    <img src={X} alt="X logo" className="h-5 w-5 hover:scale-110 cursor-pointer" />
+                </div>
+            </div>
+            <div className="w-[345px] h-[233px]  ml-4 mt-4 bg-primaryLight rounded-2xl"></div>
+
+
+        </div>
+    );
 }
 
 export default TeamCard;
--- src/components/Teamsection.js
+++ src/components/Teamsection.js
@@ -1,8 +1,12 @@
+import TeamCard from "./Teamcard";
 const TeamSection = () => {
     return (
         <div className="flex flex-col items-center justify-center mt-20">
             <div className="font-outfit text-4xl text-primary font-bold">Meet the<span className="text-secondary"> Team</span></div>
-            <div className="flex flex-row justify-between items-center gap-2 mt-10">
+            <div className="flex flex-row flex-wrap justify-between items-center gap-2 mt-32">
+                <TeamCard />
+                <TeamCard />
+                <TeamCard />
             </div>
         </div>
     );
