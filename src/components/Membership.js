6e1586403c820a096b60082891f3de6fd01039d9
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 21:55:59 +0530

    added responsiveness to the membership card for smaller screen size

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -6,13 +6,13 @@ const MembershipCard = () => {
         { title: 'Start Your Membership', logo: chevronRight }
     ];
     return (
-        <div className="flex flex-col h-[316px] bg-gradient-to-r from-secondary to-primary rounded-3xl justify-center mt-20">
-            <div className="flex flex-col mx-[90px]">
-                <div className="flex flex-col gap-6 items-center mb-12">
+        <div className="flex flex-col h-[316px] bg-gradient-to-r from-secondary to-primary rounded-3xl max-sm:rounded-[18px] justify-center mt-20 max-sm:mt-10 max-sm:h-[165px]">
+            <div className="flex flex-col mx-[90px] gap-10 max-sm:gap-3 max-sm:mx-[26px] justify-center">
+                <div className="flex flex-col gap-6 items-center  max-sm:gap-1">
                     {/* Topic */}
-                    <div className="font-outfit text-xl uppercase text-background font-semibold">Get Enrolled</div>
+                    <div className="font-outfit text-xl uppercase text-background font-semibold max-sm:text-xs">Get Enrolled</div>
                     {/* Description */}
-                    <div className="font-outfit text-4xl text-white font-bold ">Unlock a World of Opportunities with Our Membership: Join for Just 50 Rupees a Month !</div>
+                    <div className="font-outfit text-4xl text-white font-bold max-sm:text-lg">Unlock a World of Opportunities with Our Membership: Join for Just 50 Rupees a Month !</div>
                 </div>
                 <div className="flex items-start">
                 {buttonInfo.map((buttonInfo, index) => (
