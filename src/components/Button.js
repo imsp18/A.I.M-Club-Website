8745d40d78e0a52284562d58504c47fb3e9a63da
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 21:55:08 +0530

    added responsiveness to button for smaller screen sizes

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,17 +12,17 @@ const Button = ({title,logo}) => {
     };
 
     return (
-        <div className={`bg-primary flex flex-row rounded-lg items-center cursor-pointer gap-1 px-4 py-2 hover:scale-105 ease-out duration-100`}
+        <div className={`bg-primary flex flex-row rounded-lg max-sm:rounded-md items-center cursor-pointer gap-1 max-sm:px-2 max-sm:py-0  px-4 py-2 hover:scale-105 ease-out duration-100`}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
-            <div className="font-semibold text-lg text-white font-outfit">
+            <div className="font-semibold text-lg max-sm:text-[12px] text-white font-outfit">
                 {title}
             </div>
             <img
                 src={logo}
                 alt="logo"
-                className={`w-6 h-6 transform ${isHovered ? 'translate-x-1' : 'translate-x-0'}`}
+                className={` max-sm:w-4 max-sm:h-4 w-6 h-6 transform ${isHovered ? 'translate-x-1' : 'translate-x-0'}`}
             />
         </div>
     );
