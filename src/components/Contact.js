848951ca1cc65787ec98986ded393b05fabcd424
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 22:44:02 +0530

    made the contact section responsive for the smaller screen size

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,34 +30,36 @@ const Contact = (title, links) => {
                 "Membership Benefits"
             ]
         }
-    
+
     ];
     return (
-        <div className="flex flex-row justify-between items-start mt-20">
+        <div className="flex flex-row justify-between items-start mt-20 max-sm:mt-10 max-sm:flex-wrap max-sm:gap-4">
             {/* Logo & Description */}
-            <div className="flex flex-col gap-5 items-start">
-                <div className="font-outfit text-black text-2xl font-bold ">AIM CLUB</div>
-                <div className="font-outfit text-black text-xl font-normal">Empowering Education, Driving Innovation</div>
+            <div className="flex flex-col gap-5 items-start max-sm:gap-2">
+                <div className="font-outfit text-black text-2xl font-bold max-sm:text-lg">AIM CLUB</div>
+                <div className="font-outfit text-black text-xl font-normal max-sm:text-sm">Empowering Education, Driving Innovation</div>
                 <div className="flex flex-col gap-2 items-start">
-                    <div className="font-outfit text-primary text-xl font-medium">Contact Us</div>
+                    <div className="font-outfit text-primary text-xl font-medium max-sm:text-lg">Contact Us</div>
                     <div className="flex max-w-md gap-x-2">
                         <label htmlFor="email-address" className="sr-only">Email address</label>
-                        <input id="email-adress" name="email" type="email" autoComplete="email" required className="font-outfit text-black text-lg font-normal border border-black rounded-md w-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" placeholder="Enter your email" />
-                        <button type="submit" className="font-outfit text-white text-lg font-medium bg-primary hover:bg-primaryLight rounded-md px-4 py-2">Send</button>
+                        <input id="email-adress" name="email" type="email" autoComplete="email" required className="font-outfit text-black text-lg font-normal border border-black rounded-md w-full max-sm:text-sm max-sm:px-2 max-sm:py-1 py-2 px-4 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent" placeholder="Enter your email" />
+                        <button type="submit" className="font-outfit text-white text-lg font-medium bg-primary hover:bg-primaryLight rounded-md px-4 py-2 max-sm:text-sm">Send</button>
                     </div>
                 </div>
             </div>
             {/* Links */}
-            {Links.map((link, index) => (
-                <div key={index} className="flex flex-col gap-5 items-start">
-                    <div className="font-outfit text-primary text-xl font-medium">{link.title}</div>
-                    <div className="flex flex-col gap-2">
-                        {link.links.map((link, index) => (
-                            <div key={index} className="font-outfit text-black text-lg font-normal hover:underline duration-150 cursor-pointer  ">{link}</div>
-                        ))}
+            <div className="flex flex-row items-start justify-between gap-12 max-sm:gap-10 max-sm:flex-wrap">
+                {Links.map((link, index) => (
+                    <div key={index} className="flex flex-col gap-5 items-start max-sm:gap-2">
+                        <div className="flex flex-col font-outfit text-primary text-xl font-medium max-sm:text-lg ">{link.title}</div>
+                        <div className="flex flex-col items-start gap-2 max-sm:gap-1">
+                            {link.links.map((link, index) => (
+                                <div key={index} className="font-outfit text-black text-lg font-normal hover:underline duration-150 cursor-pointer max-sm:text-sm">{link}</div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
