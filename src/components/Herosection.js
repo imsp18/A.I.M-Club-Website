15e482b8ba886d202299b59c4531e990ffd03178
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 15:33:03 +0530

    made the hero section responsive for screen sm

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -7,11 +7,11 @@ const HeroSection = () => {
         {title: 'Upcoming Events', logo: chevronRight}   
       ];
     return (
-        <div className="flex flex-row mt-20 justify-center items-center">
+        <div className="flex flex-row mt-20 max-sm:mt-10 justify-center items-center max-sm:gap-10 max-sm:flex-col">
             {/* Text Part */}
-            <div className="flex flex-col items-start gap-2 w-[525px]" >
-                <h2 className="font-outfit text-primary text-4xl font-bold underline"> Empowering minds in Automation, Invention & Manufacturing </h2>
-                <p className="font-outfit text-black text-lg">Unlocking Creativity, Inspiring Innovation, and Engineering the Future</p>
+            <div className="flex flex-col items-start gap-2 max-sm:gap-4 w-[525px] max-sm:w-[310px]" >
+                <h2 className="font-outfit text-primary text-4xl font-bold underline max-sm:text-2xl"> Empowering minds in Automation, Invention & Manufacturing </h2>
+                <p className="font-outfit text-black text-lg max-sm:text-sm">Unlocking Creativity, Inspiring Innovation, and Engineering the Future</p>
                 {buttonInfo.map((buttonInfo, index) => (
                     <Button key={index} title={buttonInfo.title} logo={buttonInfo.logo} />
                 ))}
