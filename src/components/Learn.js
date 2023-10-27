12b32f238622b309074155452221285c9fca90f7
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 17:30:06 +0530

    made some changes to the learn section

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -17,8 +17,8 @@ const Learn = () => {
         {title: 'Robotics', logo:skill6},
     ];
     return (
-        <div className="flex flex-col items-center mt-20">
-            <div className="font-outfit text-4xl text-primary font-bold">Learn<span className="text-secondary"> More</span> About</div>
+        <div className="flex flex-col items-center mt-20 max-sm:mt-10">
+            <div className="font-outfit text-4xl text-primary font-bold max-sm:text-2xl">Learn<span className="text-secondary"> More</span> About</div>
             <div className="flex flex-row flex-wrap max-sm:flex-wrap justify-between items-center max-sm:gap-2 gap-10 mt-10">
                 {skills.map((skills, index) => (
                     <SkillsButton key={index} title={skills.title} logo={skills.logo} />
