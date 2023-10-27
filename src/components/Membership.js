8b8082b3cfc32ad6be09d8d028ee1a4e517f0ec5
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 23:20:33 +0530

    added more better Ux for membership card

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -6,7 +6,7 @@ const MembershipCard = () => {
         { title: 'Start Your Membership', logo: chevronRight }
     ];
     return (
-        <div className="flex flex-col h-[316px] bg-gradient-to-r from-secondary to-primary rounded-3xl max-sm:rounded-[18px] justify-center mt-20 max-sm:mt-10 max-sm:h-[165px]">
+        <div className="flex flex-col h-[316px] bg-gradient-to-r from-secondary to-primary rounded-3xl max-sm:rounded-[18px] justify-center mt-20 max-sm:mt-10 max-sm:h-[190px]">
             <div className="flex flex-col mx-[90px] gap-10 max-sm:gap-3 max-sm:mx-[26px] justify-center">
                 <div className="flex flex-col gap-6 items-center  max-sm:gap-1">
                     {/* Topic */}
