0b22768e105ae460fe173b1c4c5ee4ec0a07d11a
Shivang Parmar <shivangparmar537@gmail.com>
2023-10-27 21:32:08 +0530

    made the team card section responsive to screen size sm

--- src/components/Teamcard.js
+++ src/components/Teamcard.js
@@ -1,23 +1,23 @@
 import X from "../assests/X-logo.svg";
-import Member1 from "../assests/member-1.png"
 import Linkedin from "../assests/linkedIn-logo.svg";
 
-const TeamCard = () => {
+const TeamCard = ({memberImage, memberName, memberDescription}) => {
     return (
         <div className="flex flex-col items-center justify-center ">
-           <img src={Member1} alt="member" className="h-[169px] w-[169px] bg-white absolute z-10 mb-60 border-primaryLight border rounded-full"></img>
-            <div className="flex flex-col px-8 justify-center items-center w-[345px] h-[233px] absolute border border-primaryLight bg-white rounded-2xl">
+            {/* Member Image */}
+           <img src={memberImage} alt="memberImage" className="h-[169px] w-[169px] max-sm:h-[148px] max-sm:w-[148px] bg-white absolute z-10 mb-60 border-primaryLight border rounded-full"></img>
+            <div className="flex flex-col px-8 justify-center items-center w-[345px] max-sm:w-[304px] h-[233px] max-sm:h-[204px] absolute border border-primaryLight bg-white rounded-2xl">
                 {/* Name */}
-                <div className="font-outfit text-lg text-primaryLight font-semibold mt-12 uppercase">name</div>
+                <div className="font-outfit text-lg text-primaryLight font-semibold mt-12 uppercase">{memberName}</div>
                 {/* Description */}
-                <div className="font-outfit text-justify text-sm text-black font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+                <div className="font-outfit text-justify text-sm text-black font-normal">{memberDescription}</div>
                 {/* Social Media */}
                 <div className="flex flex-row gap-4 mt-4">
                     <img src={Linkedin} alt="X logo" className="h-5 w-5 hover:scale-110 cursor-pointer hover:co" />
                     <img src={X} alt="X logo" className="h-5 w-5 hover:scale-110 cursor-pointer" />
                 </div>
             </div>
-            <div className="w-[345px] h-[233px]  ml-4 mt-4 bg-primaryLight rounded-2xl"></div>
+            <div className="w-[345px] h-[233px] max-sm:w-[304px] max-sm:h-[204px] max-sm:ml-2 max-sm:mt-2 ml-4 mt-4 bg-primaryLight rounded-2xl"></div>
 
 
         </div>
--- src/components/Teamsection.js
+++ src/components/Teamsection.js
@@ -1,12 +1,19 @@
 import TeamCard from "./Teamcard";
+import Member1 from "../assests/member-1.png";
 const TeamSection = () => {
+    const memberInfo = [
+        { memberImage: Member1, memberName: "Name", memberDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod." },
+        { memberImage: Member1, memberName: "Name", memberDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod." },
+        { memberImage: Member1, memberName: "Name", memberDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod." }  
+    ];
+    const maxMemberToShowOnSmallScreen = 1;
     return (
-        <div className="flex flex-col items-center justify-center mt-20">
-            <div className="font-outfit text-4xl text-primary font-bold">Meet the<span className="text-secondary"> Team</span></div>
+        <div className="flex flex-col items-center justify-center mt-20 max-sm:mt-10">
+            <div className="font-outfit text-4xl text-primary font-bold max-sm:text-2xl">Meet the<span className="text-secondary"> Team</span></div>
             <div className="flex flex-row flex-wrap justify-between items-center mt-32">
-                <TeamCard />
-                <TeamCard />
-                <TeamCard />
+                {memberInfo.slice(0, maxMemberToShowOnSmallScreen).map((member, index) => (
+                    <TeamCard key={index} memberImage={member.memberImage} memberName={member.memberName} memberDescription={member.memberDescription} />
+                ))}
             </div>
         </div>
     );
